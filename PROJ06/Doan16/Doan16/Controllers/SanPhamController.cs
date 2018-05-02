bba94e603b5b1663d62dcc17fa79d2b824a67fba
khangtran97 <khangtdk0612@gmail.com>
2018-05-02 20:10:26 +0700

    PROJ06-LOADDULIEUSANPHAM-1.0.0

---
+++ PROJ06/Doan16/Doan16/Controllers/SanPhamController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Doan16.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace Doan16.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        QLCuaHangDBManage db = new QLCuaHangDBManage();
+        // GET: TrangChu
+        public List<NuocGK> Layngkmoi(int count, string name)
+        {
+            if(name!=null && name!= "")
+            {
+                return (from n in db.NuocGKs where n.tenNGK.ToUpper().Contains(name.ToUpper()) orderby n.id_NuocGK descending select n).Take(count).ToList();
+            }
+            else
+                return (from n in db.NuocGKs orderby n.id_NuocGK descending select n).Take(count).ToList();
+        }
+        public ActionResult Index(int ? page, FormCollection fc)
+        {
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            string name = fc["SearchString"];
+            var nuocgkmoi = Layngkmoi(20, name);
+            return View(nuocgkmoi.ToPagedList(pageNum, pageSize));
+        }
+
+        public ActionResult TimKiem()
+        {
+            return PartialView();
+        }
+
+
+    }
+}
